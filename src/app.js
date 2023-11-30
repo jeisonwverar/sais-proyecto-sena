7f02ce9c8d8d9b0cdd723ef8350e192352e9a927
jeisonTrabajo <jw.vera.r@gmail.com>
2023-11-30 14:04:57 -0500

    configuracion de cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ import authRoutes from './routes/auth.routes.js';
 const app=express();
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
